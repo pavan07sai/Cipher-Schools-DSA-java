b5dcc2b672cc9f59b3830ceccf253d41ec1dedc1
pavan07sai <118649187+pavan07sai@users.noreply.github.com>
2023-07-24 10:41:52 +0530

    Add files via upload

---
+++ Lecture37-Detect and Remove Loop in Linked List.java
@@ -0,0 +1,19 @@
+
+
+class Solution{
+    public static boolean detactLoop(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
